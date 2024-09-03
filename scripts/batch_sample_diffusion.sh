3bdb1535066f221865e398604f54f9fa1da1808e
mahb-THU <1799387980@qq.com>
2024-09-03 23:05:56 +0800

    fix:eval scipt

--- scripts/batch_sample_diffusion.sh
+++ scripts/batch_sample_diffusion.sh
@@ -1,21 +1,23 @@
 TOTAL_TASKS=100
-BATCH_SIZE=100
+BATCH_SIZE=50
 
-if [ $# != 2 ]; then
-    echo "Error: 2 arguments required."
+if [ $# != 5 ]; then
+    echo "Error: 5 arguments required."
     exit 1
 fi
 
 CONFIG_FILE=$1
-RESULT_PATH="experiments/$2"
-NODE_ALL=1
-NODE_THIS=0
-START_IDX=0
+RESULT_PATH=$2
+NODE_ALL=$3
+NODE_THIS=$4
+START_IDX=$5
 
-for ((i=$START_IDX;i<$TOTAL_TASKS;i++)); do
-    NODE_TARGET=$(($i % $NODE_ALL))
-    if [ $NODE_TARGET == $NODE_THIS ]; then
-        echo "Task ${i} assigned to this worker (${NODE_THIS})"
-        python -m scripts.sample_diffusion ${CONFIG_FILE} -i ${i} --batch_size ${BATCH_SIZE} --result_path ${RESULT_PATH}
-    fi
-done
+# for ((i=$START_IDX;i<$TOTAL_TASKS;i++)); do
+#     NODE_TARGET=$(($i % $NODE_ALL))
+#     if [ $NODE_TARGET == $NODE_THIS ]; then
+#         echo "Task ${i} assigned to this worker (${NODE_THIS})"
+#         python -m scripts.sample_diffusion ${CONFIG_FILE} -i ${i} --batch_size ${BATCH_SIZE} --result_path ${RESULT_PATH}
+#     fi
+# done
+
+python -m scripts.evaluate_diffusion ${RESULT_PATH} --protein_root /sharefs/share/sbdd_data/test_set --docking_mode vina_dock
